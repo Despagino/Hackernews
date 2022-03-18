6d6f6dbdb7c1f0b7d07bca09e7f44ab873df9668
Gino Tasis <gtasis84@gmail.com>
2022-03-18 13:00:11 -0400

    Created file ready to code

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by Gino Tasis on 3/18/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+            .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
+    }
+}
---
+++ HackerNews/HackerNewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  HackerNewsApp.swift
+//  HackerNews
+//
+//  Created by Gino Tasis on 3/18/22.
+//
+
+import SwiftUI
+
+@main
+struct HackerNewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
