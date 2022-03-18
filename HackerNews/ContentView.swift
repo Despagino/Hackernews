0d5dd72973f7b2a7328ed664242484a340648172
Gino Tasis <gtasis84@gmail.com>
2022-03-18 13:07:44 -0400

    Testing out the List, Navigation component and adding a navigation title

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -9,8 +9,15 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            
+            List {
+                Text("Hello, world!")
+                Text("Goodbye. world!")
+                
+            }
+            .navigationTitle("Hacker News")
+        }
     }
 }
 
