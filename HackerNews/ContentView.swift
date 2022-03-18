0e126f7bb649c6b1e5ab6b9eb913ab9c18543692
Gino Tasis <gtasis84@gmail.com>
2022-03-18 13:49:19 -0400

    Add onAppear function in our contentview which acts like the viewdidload, now triggering our fetchData function in our networkMaanger

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -24,6 +24,9 @@ struct ContentView: View {
             }
             .navigationTitle("Hacker News")
         }
+        .onAppear {
+            netWorkManager.fetchData()
+        }
     }
 }
 
