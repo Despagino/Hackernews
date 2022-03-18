bfd601a117834ea5e3f8c26741b05dc01b737cf7
Gino Tasis <gtasis84@gmail.com>
2022-03-18 13:18:00 -0400

    Implemented List to dynamically add the title I had from my array

--- HackerNews/ContentView.swift
+++ HackerNews/ContentView.swift
@@ -11,9 +11,10 @@ struct ContentView: View {
     var body: some View {
         NavigationView {
             
-            List {
-                Text("Hello, world!")
-                Text("Goodbye. world!")
+            // for every single "post" in our posts array
+            List(posts) { post in
+                
+                Text(post.title)
                 
             }
             .navigationTitle("Hacker News")
@@ -27,3 +28,20 @@ struct ContentView_Previews: PreviewProvider {
             .previewDevice(PreviewDevice(rawValue: "iPhone 13 Pro Max"))
     }
 }
+
+
+struct Post: Identifiable {
+    
+    // identifiable allows our list to recognize the order of each of your post objects based on our id
+    // for a structure to conform to identifiable it needs to have an id that is a string
+    
+    let id: String
+    let title: String
+    
+}
+
+let posts = [
+Post(id: "1", title: "Hello"),
+Post(id: "2", title: "Youre cool I guess"),
+Post(id: "3", title: "War is happening")
+]
