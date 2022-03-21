2ce3991b8e8df5ac7d1f5ef080a97c0de79bcaad
Gino Tasis <gtasis84@gmail.com>
2022-03-21 11:04:20 -0400

    Added a seperate webfiew file to include the webview struct there for refactoring

---
+++ HackerNews/Views/Webview.swift
@@ -0,0 +1,31 @@
+//
+//  Webview.swift
+//  HackerNews
+//
+//  Created by Gino Tasis on 3/21/22.
+//
+
+import Foundation
+import WebKit
+import SwiftUI
+
+struct WebView: UIViewRepresentable {
+    
+    let urlString: String?
+    
+    func makeUIView(context: Context) -> WKWebView {
+        return WKWebView()
+    }
+    
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        
+        if let safeString = urlString {
+            if let url = URL(string: safeString) {
+                let request = URLRequest(url: url)
+                uiView.load(request)
+            }
+        }
+    }
+    
+    
+}
